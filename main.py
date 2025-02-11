55eebe3ce3a0cac32fc44a84ce8bd343afb2526e
BitL8-ByteShort <51251284+BitL8-ByteShort@users.noreply.github.com>
2025-02-11 11:15:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -76,5 +76,5 @@ def check_trademark(name: str):
 
 # Run Uvicorn (Fix Port Issue for Render)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render assigns a PORT dynamically
+    port = int(os.environ.get("PORT", 8000))  # Render assigns a PORT dynamically
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
